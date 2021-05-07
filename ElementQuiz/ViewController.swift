f7bb34fd2aafb87dd1aadca6b1350976f853c884
Conrad Flescher <conrad.flescher@gmail.com>
2021-05-06 17:15:55 -0700

    Initial commit -- add image assets and basic UI code

--- ElementQuiz/ViewController.swift
+++ ElementQuiz/ViewController.swift
@@ -5,13 +5,53 @@
 
 import UIKit
 
+enum InterfaceMode {
+    case flashCards
+    case quiz
+}
+
 class ViewController: UIViewController {
 
+    var mode: InterfaceMode = .flashCards
+    
+    let elementList = ["Carbon", "Gold", "Chlorine", "Sodium"]
+    /// Index from `elementList` of what element is currently being displayed on screen.
+    var currentElementIndex = 0
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var answerLabel: UILabel!
+    
+    @IBOutlet weak var modeSelector: UISegmentedControl!
+    @IBOutlet weak var textField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateElement()
     }
-
-
+    
+    func updateElement() {
+        let elementName = elementList[currentElementIndex]
+        let image = UIImage(named: elementName)
+        imageView.image = image
+        
+        answerLabel.text = "?"
+    }
+    
+    @IBAction func showAnswerButtonClicked(_ sender: UIButton) {
+        answerLabel.text = elementList[currentElementIndex]
+    }
+    
+    @IBAction func nextButtonClicked(_ sender: UIButton) {
+        currentElementIndex += 1
+        
+        if currentElementIndex >= elementList.count {
+            currentElementIndex = 0
+        }
+        
+        updateElement()
+    }
+    
+    
 }
 
